a827f0e53fa7d4f755ad26ff859b7329ceee381e
Isaac Gil <isaacgil325@gmail.com>
2022-07-01 20:14:22 +0000

    Ready for data to be converted into sql database

--- job_search.py
+++ job_search.py
@@ -23,9 +23,15 @@ location = input("(OPTIONAL) Enter a location for jobs, else hit enter: ").strip
 # while not is_valid_input(job_field):
 #     job_field = input("Enter comma-separated fields you would like to search for jobs: ")
 
+#interchange use of API Keys to limit searches since we only get 100 each
 API_KEYS = ('e21193f2b2ee7a0a7042c7a414822b20b10c84609c42a408732401d8b62ddc06',
             '9e8e77e8075bf5f1bfbbef8848ba3b735d1cf01e0490877307eded9945e41777')
 
 counter = random.randint(0, 1) #?
 
-# r = requests.get()
+#make GET request and convert to json data containing job results
+r = requests.get(f'https://serpapi.com/search.json?engine=google_jobs&q={job_fields}&location={location}&api_key={API_KEYS[counter]}')
+data = r.json()['jobs_results']
+
+#Now that we have list of dictionaries with seperate job offerings data, we must now convert it into sql database
+
