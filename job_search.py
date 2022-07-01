7370b1faa0c498e1351749bece5127d7e1a8a8b3
Joshua Feliciano <joshfeli613@gmail.com>
2022-07-01 18:41:23 +0000

    Set up basic infrastructure for API call

---
+++ job_search.py
@@ -0,0 +1,31 @@
+# start with the imports
+import pandas as pandas
+import random
+import requests
+import json
+import SQLAlchemy as db
+
+# using SerpAPI -- no import needed, just the keys?
+
+job_fields = input("Enter comma-separated fields you would like to search for jobs: ")
+location = input("(OPTIONAL) Enter a location for jobs, else hit enter: ").strip()
+
+
+# # validate job_field input formatting
+# def is_valid_input(job_field):
+#     if not ('a' < job_field < 'z' or 'A' < job_field < 'Z':
+#         print("Improper formatting")
+#         return False
+
+# leave input as-is: API takes care of weird inputs with an "error" key in JSON
+
+
+# while not is_valid_input(job_field):
+#     job_field = input("Enter comma-separated fields you would like to search for jobs: ")
+
+API_KEYS = ('e21193f2b2ee7a0a7042c7a414822b20b10c84609c42a408732401d8b62ddc06',
+            '9e8e77e8075bf5f1bfbbef8848ba3b735d1cf01e0490877307eded9945e41777')
+
+counter = random.randint(0, 1) #?
+
+# r = requests.get()
