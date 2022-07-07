9a1c5708e753f335806d50a96634df34d13186d4
Isaac Gil <isaacgil325@gmail.com>
2022-07-07 19:18:20 +0000

    coding integration
    Co-authored-by: Josh Feliciano <joshfeli613@gmail.com>

--- job_search.py
+++ job_search.py
@@ -8,10 +8,31 @@ import sqlalchemy as db
 import pdb
 from sqlalchemy.types import String
 
+# interchange use of API Keys to limit searches
+API_KEYS = ('e21193f2b2ee7a0a7042c7a414822b20b10c84609c42a408732401d8b62ddc06',
+            '9e8e77e8075bf5f1bfbbef8848ba3b735d1cf01e0490877307eded9945e41777')
+
+key_index = random.randint(0, 1)
+
+
 def print_func(query):
     df = pd.DataFrame(query)
     df.columns = ['user_id', 'title', 'company_name', 'location', 'via', 'description', 'job_id', 'detected_extensions.posted_at', 'detected_extensions.schedule_type']
     pprint.pprint(df)
+
+def print_links(id_list):
+        list_data = []
+        for id in id_list:
+            request = requests.get(f'https://serpapi.com/search.json?\
+engine=google_jobs_listing&q={id}&api_key={API_KEYS[key_index]}')
+            link_data = request.json()["apply_options"]
+            list_data.append(link_data)
+        for i in range(len(list_data)):
+            print(f'job {i + 1}:')
+            for j in range(len(list_data[i])):
+                for key, value in list_data[i][j].items():
+                    if key == 'link' and j <= 3:
+                        print(f'Application Link {j}: {list_data[i][j][key]}')
 
 def user_check(user_name):
     if user_name == None:
@@ -33,7 +54,7 @@ def enter_into_database(data, user):
     print(name_list)
     # print(data_table)
     engine = db.create_engine('sqlite:///job-search-results.db')
-    data_table.to_sql('jobs', con=engine, if_exists='replace', index=False)#need to fix
+    data_table.to_sql('jobs', con=engine, if_exists='append', index=False)#need to fix
     query = engine.execute(f"SELECT * FROM jobs WHERE user_id='{user}'").fetchall()
     print_func(query)
     return engine
@@ -44,8 +65,11 @@ def program_driver(user_name):
     if num_user > 0:
         print(f"Welcome back, {user_name.replace('_', ' ').title()}!")
         query = engine.execute(f"SELECT * FROM jobs WHERE user_id='{user_name}';")
+        id_list = engine.execute(f"SELECT job_id FROM jobs WHERE user_id='{user_name}';").fetchall()
+        id_list = [id[0] for id in id_list]
         print_func(query)
-        answer = input("If you would like to research different jobs, press 'y' and hit enter. Otherwise, hit enter.").lower().strip()
+        print_links(id_list)
+        answer = input("If you would like to research different jobs, press 'y' and hit enter. Otherwise, hit enter: ").lower().strip()
         if answer == 'y':
             search_api()
     else:
@@ -60,19 +84,14 @@ in which you would like to search for jobs: ").strip()
     location = input("(OPTIONAL) Enter a location for jobs,\
 else hit enter: ").strip()
 
-    # interchange use of API Keys to limit searches
-    API_KEYS = ('e21193f2b2ee7a0a7042c7a414822b20b10c84609c42a408732401d8b62ddc06',
-                '9e8e77e8075bf5f1bfbbef8848ba3b735d1cf01e0490877307eded9945e41777')
-
-    key_index = random.randint(0, 1)
-
+    
     # make GET request and convert to json data containing job results
     r = requests.get(f'https://serpapi.com/search.json?engine=google_jobs&q={job_fields}&location={location}&api_key={API_KEYS[key_index]}')
     data = r.json()['jobs_results']
     for job in data:
         job.pop('extensions', None)
         job.pop('thumbnail', None)
-    pprint.pprint(data)
+    pprint.pprint(data[:5])
 
     job_nums = input("type the number of the job you are interested in. \
 (Number meaning what place in the order shown) If you are interested \
@@ -82,23 +101,9 @@ in multiple, seperate numbers with a ',': ")
     dict_list = []
     for num in nums:
         id_list.append(data[int(num) - 1]['job_id'])
-        dict_list.append(data[int(num) - 1])
+        dict_list.append(data[int(num) - 1])   
     enter_into_database(dict_list, user_name)
-
-    list_data = []
-    # for id in id_list:
-    #     request = requests.get(f'https://serpapi.com/search.json?\
-    #     engine=google_jobs_listing&q={id}&api_key={API_KEYS[key_index]}')
-    #     link_data = request.json()["apply_options"]
-    #     list_data.append(link_data)
-    # for i in range(len(list_data)):
-    #     print(f'job {i + 1}:')
-    #     for j in range(len(list_data[i])):
-    #         for key, value in list_data[i][j].items():
-    #             if key == 'link' and j <= 3:
-    #                 print(f'Application Link {j}: {list_data[i][j][key]}')
-
-
+    print_links(id_list) 
 
 if __name__ == '__main__':
     user_name = None
--- temp_script.py
+++ temp_script.py
@@ -1,8 +1,11 @@
 import sqlalchemy as db
 import pandas as pd
+import pprint
 
 engine = db.create_engine('sqlite:///job-search-results.db')
 
-query = engine.execute('.tables;').fetchall()
-
-print(query)
+#query = engine.execute('.tables;').fetchall()
+id_list = engine.execute(f"SELECT job_id FROM jobs WHERE user_id='joshua_feliciano';").fetchall()
+id_list = [id[0] for id in id_list]
+pprint.pprint(id_list)
+#print(query)
